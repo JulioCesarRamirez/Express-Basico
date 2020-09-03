7c71f28afaeff2672d43bae44c4d17d6261f1941
Julio Cesar Ramirez Jimenez <julio_marshal@hotmail.com>
2020-09-02 20:26:29 -0500

    tickets app prod

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,15 +1,36 @@
 const { io } = require("../server");
+const { TicketControl } = require("../classes/ticket-control");
+
+const ticketControl = new TicketControl();
+
 io.on("connection", (client) => {
-  console.log("Usuario conectado");
-  client.emit("enviarMensaje", {
-    usuario: "Admin",
-    mensaje: "Bienvenido a esta app",
+  client.on("siguienteTicket", (data, callback) => {
+    let siguiente = ticketControl.siguiente();
+
+    console.log(siguiente);
+    callback(siguiente);
   });
-  client.on("disconnect", () => {
-    console.log("Usuario desconectado");
+
+  client.emit("estadoActual", {
+    actual: ticketControl.getUltimoTicket(),
+    ultimos4: ticketControl.getUltimos4(),
   });
-  client.on("enviarMensaje", (data, callback) => {
-    console.log(data);
-    client.broadcast.emit("enviarMensaje", data);
+
+  client.on("atenderTicket", (data, callback) => {
+    if (!data.escritorio) {
+      return callback({
+        err: true,
+        mensaje: "El escritorio es necesario",
+      });
+    }
+
+    let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+    callback(atenderTicket);
+
+    // actualizar/ notificar cambios en los ULTIMOS 4
+    client.broadcast.emit("ultimos4", {
+      ultimos4: ticketControl.getUltimos4(),
+    });
   });
 });
